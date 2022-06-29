a8e713698a3734da50a4bc317b2586d756ed61e7
piyush01123 <piyush.singh@research.iiit.ac.in>
2022-06-29 14:16:11 +0530

    minor edit

--- setup.py
+++ setup.py
@@ -1,9 +1,13 @@
 from setuptools import setup, find_packages
 setup(
 	name="RCML",
-	version="0.1",
+	version="1.1",
 	package_dir={'RCML':'src'},
 	packages=['RCML'],
 	install_requires=['pandas','numpy','scikit-learn','matplotlib','cvxpy'],
+	license=open('LICENSE').read(),
+	description="Minimal implementations of classical ML algorithms",
+	long_description=open("README.md").read(),
+	long_description_content_type='text/markdown',
 	author="Piyush Singh"
 )
